0124303e0faa60ee0b4fe7736e82bcbd805941f4
Peter Goshulak <pgoshulak@gmail.com>
2018-05-24 20:46:22 -0400

    Add http basic auth to admin routes

---
+++ app/controllers/concerns/http_auth_concern.rb
@@ -0,0 +1,13 @@
+module HttpAuthConcern
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :http_authenticate
+  end
+
+  def http_authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['ADMIN_USERNAME'] && password == ENV['ADMIN_PASSWORD']
+    end
+  end
+end
