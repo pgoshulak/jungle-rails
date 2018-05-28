5c701e713ee637334c2ba49d7348aa625f5c4f35
Peter Goshulak <pgoshulak@gmail.com>
2018-05-28 12:50:21 -0400

    Add tests for invalid product creation

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe Product, type: :model do
       it 'saves a valid Product successfully' do
         cat = Category.create! name: 'Things'
         cat.products.create name: 'Widget',
-                            description: 'This is a widget',
                             quantity: 5,
                             price: 1000
 
@@ -15,7 +14,47 @@ RSpec.describe Product, type: :model do
     end
 
     context 'with an invalid Product' do
-      # pending 'raises error for invalid name'
+      
+      it 'raises error for missing name' do
+        cat = Category.create! name: 'Things'
+        p = cat.products.create quantity: 5,
+                                price: 1000
+        expect(Product.all).to be_empty
+        expect(p.errors.full_messages).to include("Name can't be blank")
+      end
+      
+      it 'raises error for missing category' do
+        p = Product.create name: 'Widget',
+                                quantity: 5,
+                                price: 1000
+        expect(Product.all).to be_empty
+        expect(p.errors.full_messages).to include("Category can't be blank")
+      end
+
+      it 'raises error for missing quantity' do
+        cat = Category.create! name: 'Things'
+        p = cat.products.create name: 'Widget',
+                                price: 1000
+        expect(Product.all).to be_empty
+        expect(p.errors.full_messages).to include("Quantity can't be blank")
+      end
+
+      it 'raises error for missing price' do
+        cat = Category.create! name: 'Things'
+        p = cat.products.create name: 'Widget',
+                                quantity: 5
+        expect(Product.all).to be_empty
+        expect(p.errors.full_messages).to include("Price can't be blank")
+      end
+
+      it 'raises error for non-number price' do
+        cat = Category.create! name: 'Things'
+        p = cat.products.create name: 'Widget',
+                                quantity: 5,
+                                price: 'five'
+        expect(Product.all).to be_empty
+        expect(p.errors.full_messages).to include("Price is not a number")
+      end
     end
   end
 end
