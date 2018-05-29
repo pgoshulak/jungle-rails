c2eae9419601ad0951da6204098b1377fa3349ac
Peter Goshulak <pgoshulak@gmail.com>
2018-05-29 15:33:32 -0400

    Add product details feature spec

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+RSpec.feature "Visitor navigates to product details page from home", type: :feature, js: true do
 
   # SETUP
   before :each do
@@ -18,14 +18,22 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
   end
   
   
-  scenario  "They see all products" do
+  scenario  "They navigate" do
     # ACT
     visit root_path
+    first_title = page.first('h4')
+    # store name of product for later verification
+    product_name = first_title.text
+    first_title.click
+    
 
     # DEBUG
+    # sleep 2
     # save_screenshot
+    # puts page.html
     
     # VERIFY
-    expect(page).to have_css 'article.product', count: 10
+    expect(page).to have_css 'section.products-show', count: 1
+    expect(page).to have_text product_name
   end
 end
